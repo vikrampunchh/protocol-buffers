2b17a3f085cc48e94b2aca8bf36f947e52348015
Vikram Biwal <vikram@punchh.com>
2020-02-03 18:24:32 +0530

    Init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+//"github.com/punchh/protocol-buffers/src/simple"
+
+import (
+	example_simple "github.com/punchh/protocol-buffers/src/simple"
+)
+
+
+func main() {
+	doSimple()
+}
+
+func doSimple() {
+
+}
